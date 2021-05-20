e51fe766b9eb87d1618ab233442ac0e69acf6b6c
tpaporkova <82776521+tpaporkova@users.noreply.github.com>
2021-05-20 04:31:26 +0300

    Create graphics3D.m

---
+++ graphics3D.m
@@ -0,0 +1,43 @@
+[x,y] = meshgrid(-5:0.01:5,-5:0.01:5);
+a = 12; b = 34; c = 56; d = 78;
+z = -(a*x + b*y + d)/c;
+surf(x,y,z)
+  
+%%
+[x,y] = meshgrid(-5:0.01:5,-5:0.01:5);
+a = 19;
+z = a.*sin(sqrt(x.^2 + y.^2))./sqrt(x.^2 + y.^2);
+plot3(x,y,z)
+  
+%%
+[x,y] = meshgrid(-5:0.01:5,-5:0.01:5);
+z = -x.*sin(x) - y.*cos(y);
+mesh(x,y,z)
+  
+%%
+[x,y] = meshgrid(-5:0.01:5,-5:0.01:5);
+z = a.*x.*exp(-x.^2 - y.^2);
+surf(x,y,z)
+  
+%%
+[u,v] = meshgrid(0:0.5:10,0:pi/12:2*pi);
+a = 33; b = 55;
+x = (a + b.*cos(v)).*cos(u);
+y = (a + b.*cos(v)).*sin(u);
+z = b.*sin(v);
+surfc(x,y,z)
+  
+%%
+[u,v] = meshgrid(0:pi/12:2*pi,-pi/2:pi/12:pi/2);
+a = 12; b = 6;
+x = a.*cos(u).*cos(v);
+y = a.*sin(u).*cos(v);
+z = b.*sin(v);
+mesh(x,y,z)
+  
+%%
+[x,y] = meshgrid(-1:0.01:1,-1:0.01:1);
+a = pi;
+r = sqrt(x.^2 + y.^2);
+z = ((2.*besselj(1,a.*r)./r)).^2;
+mesh(x,y,z)
