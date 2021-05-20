8aef5836843c420f0877bf71f921bce1343132d8
tpaporkova <82776521+tpaporkova@users.noreply.github.com>
2021-05-20 04:25:44 +0300

    Update symb.m

--- symb.m
+++ symb.m
@@ -5,6 +5,7 @@ factor(P)
 syms x y
 P = x^3+x^2*y+x^2-2*x*y^2+x*y-2*y^2;
 factor(P)
+
 %%
 syms x
 f = x*exp(-x);
@@ -17,6 +18,7 @@ limit(f,x,pi/4)
 syms x
 f = (7*x^3+x^2-1)/(5*x^2+2*x+1);
 limit(f,x,inf)
+
 %%
 syms x
 simplify(diff(atan(x)/2 - x./(2*(1+x.^2).^2)))
@@ -26,6 +28,7 @@ simplify(diff(3*x^4 - 14*x^3+12*x^2+24*x+6))
 %%
 syms x
 simplify(diff(1/6*log((x-3)/(x+3))))
+
 %%
 syms x
 int((sqrt(1-x^2))/x^2)
@@ -35,9 +38,11 @@ int(1/sin(x)^3)
 %%
 syms x
 int(((sin(2*x)+1)^(1/7))*cos(2*x))
+
 %%
 syms n
 symsum(1/(n+1)^2,n,0,inf)
+
 %%
 syms x
 f = exp(x);
@@ -50,6 +55,7 @@ f = sin(x)^2;
 t = taylor(f,x,0,'Order',11);
 disp(t);
 taylortool(f)
+
 %%
 syms t u
 x = t*cos(u)*cos(u);
