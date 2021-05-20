e891f50ec83d31e108557014e7126d9bf1ae733a
tpaporkova <82776521+tpaporkova@users.noreply.github.com>
2021-05-20 05:15:52 +0300

    Update graphics3D.m

--- graphics3D.m
+++ graphics3D.m
@@ -33,6 +33,23 @@ a = 12; b = 6;
 x = a.*cos(u).*cos(v);
 y = a.*sin(u).*cos(v);
 z = b.*sin(v);
+
+%%
+a = -3; c = 5;
+[alpha,beta] = meshgrid(-1:0.1:1,0:0.1:2*pi);
+x = a*cosh(alpha).*cos(beta);
+y = a*cosh(alpha).*sin(beta);
+z = c*sinh(alpha);
+surf(x,y,z)
+
+%%
+p = 7;
+[r,beta] = meshgrid(0:1:10,0:0.1:2*pi);
+x = r.*cos(v);
+y = r.*sin(v);
+z = r.^2./(2*p);
+surf(x,y,z)
+
 mesh(x,y,z)
   
 %%
