cf112736b9e0a16b6b2c061c5ec525764438b55e
tpaporkova <82776521+tpaporkova@users.noreply.github.com>
2021-05-26 03:18:29 +0300

    Update symb.m

--- symb.m
+++ symb.m
@@ -67,3 +67,39 @@ Y = [diff(x) diff(z); diff(x,2) diff(z,2)];
 Z = [diff(x) diff(y); diff(x,2) diff(y,2)];
 kruch = det(A)/(det(X)^2+det(Y)^2+det(Z)^2)
 kriv = (det(X)^2+det(Y)^2+det(Z)^2)/(diff(x)^2+diff(y)^2+diff(z)^2)^3
+
+
+%% UPDATED
+syms x
+y = exp(1/(1-x^2))/(1+x^2);
+k = limit(y/x,inf);
+b = limit(y-k*x,inf);
+asymptotes_hor = k*x+b;
+ezplot(y,-7,7)
+grid on
+hold on
+ezplot(asymptotes_hor)
+xline(-1,'--r') % vertical
+xline(1,'--r')  % asymptotes
+extrema = solve(diff(y));
+for i = 1:length(extrema)
+    plot(extrema(i),subs(y,x,extrema(i)),'*','Color','green')
+end
+inflections = real(solve(diff(y,2)));
+for j = 1:length(inflections)
+    plot(inflections(j),subs(y,x,inflections(j)),'*','Color','black')
+end
+hold off
+
+%%
+syms x
+y = sin(x)/(x^2+1);
+ezplot(y,-5,5)
+grid on
+hold on
+y1 = matlabFunction(y);
+[x_m,y_m] = fminbnd(y1,-5,5);
+disp([x_m,y_m]);
+disp([-x_m,-y_m]);
+plot(x_m,y_m,'*',-x_m,-y_m,'*')
+hold off
